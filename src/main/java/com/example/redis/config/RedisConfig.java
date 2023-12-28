ba01640513ffc8e0451dac6cd828f70203806d24
CMHyuk <alsgur9710@naver.com>
2023-12-28 14:31:13 +0900

    redis

--- src/main/java/com/example/redis/config/RedisConfig.java
+++ src/main/java/com/example/redis/config/RedisConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -18,10 +18,10 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<Long, String> redisTemplate() {
-        RedisTemplate<Long, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
+    public StringRedisTemplate redisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return stringRedisTemplate;
     }
 
 }
--- src/main/java/com/example/redis/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/redis/repository/RefreshTokenRepository.java
@@ -14,14 +14,14 @@ public class RefreshTokenRepository {
     private final RedisTemplate redisTemplate;
 
     public void saveRefreshToken(Long memberId, String refreshToken) {
-        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(memberId, refreshToken);
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(String.valueOf(memberId), refreshToken);
         log.info("=======레디스에 저장========");
     }
 
     public String findByMemberId(Long memberId) {
-        ValueOperations<Long, String> valueOperations = redisTemplate.opsForValue();
-        String refreshToken = valueOperations.get(memberId);
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        String refreshToken = valueOperations.get(String.valueOf(memberId));
         log.info("refreshToken = {} ", refreshToken);
         if (refreshToken.equals("") || refreshToken == null) {
             throw new IllegalArgumentException("존재하지 않는 토큰입니다.");
